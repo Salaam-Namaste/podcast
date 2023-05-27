222896b3ad9d571b57daa5e05ce8dde39fd6eb27
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-05-27 21:34:52 +0530

    added dynamic buttons

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let playBtn=document.getElementById('play-btn')
-    let isPause=true
-
-    // changing the button on click
-    playBtn.addEventListener('click',function(){
-        if(isPause==true){
-            playBtn.src='assets/png/play-button.png'
-            isPause=false
-        }
-        else{
-            playBtn.src='assets/png/pause.png'
-            isPause=true
-        }
-
-    })
-
-    
+    let isPause=true;
+    const buttons = document.getElementsByClassName('play-btn');
 
 
+    for (let i = 0; i <=buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+
+            if(isPause==true){
+                buttons[i].src='assets/png/play-button.png'
+                isPause=false
+            }
+            else{
+                buttons[i].src='assets/png/pause.png'
+                isPause=true
+            }
+
+        });
+    }
 
 
   });
