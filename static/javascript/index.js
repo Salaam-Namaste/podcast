4e0d49a7e7e2bad674cd1baee9291fc00c2869e6
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-05-28 14:54:34 +0530

    ui updates

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
       let audio1 = document.getElementById("audio1");
       let info=document.getElementById("info1");
       let infotext=document.getElementById('info-text')
+      let grid=document.getElementById('grid-container')
+      let podSection=document.getElementById('podcast-section')
 
       audio1.addEventListener('play',function(){
         info.classList.add('show-info')
@@ -10,11 +12,28 @@ document.addEventListener('DOMContentLoaded', function() {
         infotext.innerText='Now Playing..'
         infotext.style.animation='yaxis-animation .5s .8s backwards'
         infotext.style.opacity=1
+
+        setTimeout(() => {
+          podSection.style.backgroundColor='rgba(0, 0, 0, 0.8)'
+        }, 200);
+
+        // setTimeout(() => {
+        //   podSection.style.width='100%'
+        //   podSection.style.marginTop='0'
+        //   podSection.style.borderRadius='0'
+        // }, 1600);
       })
 
       audio1.addEventListener('pause',function(){
-        info.classList.remove('show-info')
+
         infotext.style.animation=''
+        
+        podSection.style.backgroundColor='rgb(233, 235, 241)'
+
+        setTimeout(() => {
+          info.classList.remove('show-info')
+        }, 200);
+
       })
 
 
