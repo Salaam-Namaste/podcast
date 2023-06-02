a468645ba07c7a829bfbf66c874771e3411644f7
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-06-03 00:09:55 +0530

    navbar updates

--- static/javascript/responsiveNavbar.js
+++ static/javascript/responsiveNavbar.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let menu=document.getElementById('menu')
+    let menuIcon=document.getElementById('menu')
     let navbar=document.getElementById('navbar')
     let overlay=document.getElementById("overlay")
 
     // for opening and closing the navigation menu
-    menu.addEventListener('click',function(){
+    menuIcon.addEventListener('click',function(){
       navbar.classList.toggle('open-navbar')
       overlay.classList.toggle('show-overlay')
+      menuIcon.classList.toggle('bx-x')
     })
+
+    window.onscroll = function() {
+        menuIcon.classList.remove('bx-x')
+        navbar.classList.remove('open-navbar')
+        overlay.classList.remove("show-overlay")
+      };
 });
